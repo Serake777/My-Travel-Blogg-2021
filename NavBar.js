d7f1d5084f5cbc216ea916ff80bf14b81915215d
Serake Mongwe <68996076+Serake777@users.noreply.github.com>
2022-01-05 13:56:54 +0200

    NavBar.js

---
+++ NavBar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function NavBar() {
+    return(
+        <nav className="nav-items">
+            <img src="./images/outline-flight.png" className ="nav-logo"/>
+            <h2 className ="nav-text">my travel journal</h2>
+        </nav>
+    )
+}
