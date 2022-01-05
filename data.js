fee69d3cd98c423b171a783df3f9837c256d02fc
Serake Mongwe <68996076+Serake777@users.noreply.github.com>
2022-01-05 13:58:15 +0200

    data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        title: "KumbuKumbu Lodge",
+        location: "Cape Town",
+        googleMapsUrl: "https://g.page/kumbukumbu-lodge?share",
+        startDate: "29 Mar, 2021",
+        endDate: "31 Mar, 2021",
+        description: "The Summit mountain villa is nestled high up on the mountain; embraced by a rocky ridge on the northern side, a valley on the eastern side and in the further south the never-ending Langeberg mountain range.",
+        imageUrl: "kumbu-kumbu.jfif"
+    },
+    {
+        title: "Dunkeld Country & Equestrian Estate",
+        location: "Mpumalanga",
+        googleMapsUrl: "https://goo.gl/maps/uXDPmhchsga1vUB96",
+        startDate: "10 Aug, 2021",
+        endDate: "12 Aug, 2021",
+        description: "Dunkeld Country & Equestrian Estate is synonymous with peace and tranquility. With breathtaking views surrounding you, featuring highlights such as the sky-scraping Blue Gum forest, pristine fly-fishing lakes, manicured gardens and the sprawling mountain range.",
+        imageUrl: "dunkeld.jpg"
+    },
+    {
+        title: "Franschhoek Wine Tram",
+        location: "Franschhoek",
+        googleMapsUrl: "https://goo.gl/maps/AqtN7NJdf2Gh4Ma38",
+        startDate: "27 Mar, 2021",
+        endDate: "28 Mar, 2021",
+        description: "THE BEST WAY TO VIEW THE CAPE WINELANDS! Hop on board and discover the beauty of the Franschhoek Valley in the best way possible! We can’t wait to share this unforgettable journey with you.",
+        imageUrl: "wine-farm.jpg"
+    }
+]
